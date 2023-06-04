0de79961d95408d19bd5c040d6f428aef0f6c2bf
aryanpalave <71414481+aryanpalave@users.noreply.github.com>
2023-06-04 16:14:56 -0700

    Update unified_dataset.R

--- unified_dataset.R
+++ unified_dataset.R
@@ -154,70 +154,7 @@ ggplot(data_salary_progression, aes(x=Career_Stage, y=Avg_Salary, fill=College_T
        y="Average Salary",
        fill="Type of College")
 
-# Shiny interactive page create:
 
-ui <- fluidPage(
-  titlePanel("Contrast: Different Stages of Earnings and Tuition Between Public and Private Colleges"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      h3("Explore the data:"),
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Background Information", 
-                 h4("The Contrasting Worlds of Public and Private Colleges:"),
-                 textOutput("overview")),
-        
-        tabPanel("Tuition and Starting Salary Comparison", 
-                 h4("Breaking Down the Dollars: Tuition Costs and Starting Salaries"),
-                 p("Through the chart, we can see that although public universities generally charge higher fees for out state students, regardless of the group compared, the tuition fees are far lower than those of private universities without discrimination. The high tuition fees of private universities often become a reason for many people to question their investment returns."),
-                 plotOutput("comp_plot")),
-        
-        tabPanel("Career Progression", 
-                 h4("Career Trajectory: The Value of Our Degree Between Private and Public Colleges Over Time"),
-                 p("When we shift our attention to the salary increase of graduates through their entire care, another interesting aspect emerges Although private university graduates initially had a slim advantage in starting salary and this makes the investment return of private colleges looks not really great, in the middle of their care, this gap because even greater This observation has been promoted us to have a more positive view of the investment returns provided by private universities, but one factor that people should also consider is that the impact of graduating from a university on personal income is usually the greatest in the early stages of their career."),
-                 plotOutput("career_plot")),
-        
-        tabPanel("Takeaways",
-                 h4("Why Should We Care: The Implications of Our Findings"),
-                 textOutput("takeaways"))
-      )
-    )
-  )
-)
-
-server <- function(input, output) {
-  
-  output$overview <- renderText({
-    paste("This data story utilizes two primary datasets: 'salaries-by-region.csv' and 'tuition_cost.csv'. Our data is categorized by public and private institutions, enabling us to highlight the cost, receive, and return on investment between these two types of colleges. Notably, while private colleges tend to have a standard tuition rate for all students, public colleges often differ in their in-state and out-of-state rates. However, this part is not the primary focus of our narrative here. Instead, we are interested in the contrast directly between private and public colleges for individuals, including the tuition cost, and starting and mid-career median salaries across these different institution types. We believe this contrast provides significant insights, as the impact of an institution's type on salary tends to diminish as one's career progresses.")
-  })
-
-  output$comp_plot <- renderPlot({
-    ggplot(data_tuition, aes(x=College_Type, y=Avg_Tuition, fill=Tuition_Type)) +
-      geom_bar(stat="identity", position="dodge") +
-      theme_minimal() +
-      labs(title="Comparison of Average Tuition Costs",
-           x="Type of College",
-           y="Average Tuition Cost",
-           fill="Tuition Type")
-  })
-
-  output$career_plot <- renderPlot({
-    ggplot(data_salary_progression, aes(x=Career_Stage, y=Avg_Salary, fill=College_Type)) +
-      geom_bar(stat="identity", position="dodge") +
-      theme_minimal() +
-      labs(title="Comparison of Average Salaries at Different Career Stages",
-           x="Career Stage",
-           y="Average Salary",
-           fill="Type of College")
-  })
-  
-  output$takeaways <- renderText({
-    paste("Education investment is an important decision that affects an individual's future financial situation. We would like to describe a comparison of tuition costs and income at different career stages after graduation between private and public universities, so that we can elucidate the value and potential returns of these investments, and provide valuable insights for future students, families, and policy makers seeking to improve higher education opportunities and social equity")
-  })
-}
 
 
 
